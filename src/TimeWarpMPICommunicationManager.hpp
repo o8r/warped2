7305e5cd1d4573815ab2a3b04bd417085fb8ae14
o8r <my+github@o8r.info>
2016-03-16 19:24:26 +0900

    add save/load functions to TimeWarpMPICommunicationManager

--- src/TimeWarpMPICommunicationManager.hpp
+++ src/TimeWarpMPICommunicationManager.hpp
@@ -8,6 +8,7 @@
 
 #include "TimeWarpCommunicationManager.hpp"
 #include "TimeWarpKernelMessage.hpp"
+#include "serialization.hpp"
 
 namespace warped {
 
@@ -71,17 +72,90 @@ private:
 
     std::shared_ptr<MessageQueue> send_queue_;
     std::shared_ptr<MessageQueue> recv_queue_;
+
+    friend class cereal::access;
+    template <typename Archive>
+    void save(Archive& ar) const {
+      ar(max_buffer_size_, max_aggregate_,
+	 cereal::base_class<TimeWarpCommunicationManager>(this),
+	 num_processes_, my_rank_);
+
+      for (int i=0; i<num_processes_; ++i)
+	ar(aggregate_messages_[i]);
+      for (int i=0; i<num_processes_; ++i)
+	ar(aggregate_message_count_by_receiver_[i]);
+
+      ar(send_queue_, recv_queue_);
+    }
+    template <typename  Archive>
+    void load(Archive& ar) {
+      auto prev_num_processes = num_processes_;
+
+      ar(max_buffer_size_, max_aggregate_, 
+	 cereal::base_class<TimeWarpCommunicationManager>(this),
+	 num_processes_, my_rank_);
+
+      if (num_processes_ != prev_num_processes) {
+	aggregate_messages_ = make_unique<std::list<std::unique_ptr<TimeWarpKernelMessage> >[]>(num_processes_);
+	aggregate_message_count_by_receiver_ = make_unique<unsigned int[]>(num_processes_);
+      }
+
+      for (int i=0; i<num_processes_; ++i)
+	ar(aggregate_messages_[i]);
+      for (int i=0; i<num_processes_; ++i)
+	ar(aggregate_message_count_by_receiver_[i]);
+
+      ar(send_queue_, recv_queue_);
+    }
+    template <typename Archive>
+    static void load_and_construct(Archive& ar, cereal::construct<TimeWarpMPICommunicationManager>& construct) {
+      unsigned int max_buffer_size, max_aggregate;
+
+      ar(max_buffer_size, max_aggregate);
+      construct(max_buffer_size, max_aggregate);
+      ar(cereal::base_class<TimeWarpCommunicationManager>(construct.ptr()));
+      ar(construct->num_processes_, construct->my_rank_);
+
+      construct->aggregate_messages_ = make_unique<std::list<std::unique_ptr<TimeWarpKernelMessage> >[]>(construct->num_processes_);
+      for (int i=0; i<construct->num_processes_; ++i)
+	ar(construct->aggregate_messages_[i]);
+      construct->aggregate_message_count_by_receiver_ = make_unique<unsigned int[]>(construct->num_processes_);
+      for (int i=0; i<construct->num_processes_; ++i)
+	ar(construct->aggregate_message_count_by_receiver_[i]);
+
+      ar(construct->send_queue_, construct->recv_queue_);
+    }
 };
 
 struct PendingRequest {
-    PendingRequest(std::unique_ptr<uint8_t[]> buffer, unsigned int count) :
-        buffer_(std::move(buffer)), count_(count) {}
+    PendingRequest(std::unique_ptr<uint8_t[]> buffer, std::size_t size, unsigned int count) :
+      buffer_(std::move(buffer)), size_(size), count_(count) {}
 
     std::unique_ptr<uint8_t[]> buffer_;
+    std::size_t size_;
     MPI_Request request_;
     int flag_;
     MPI_Status status_;
     int count_;
+
+    template <typename Archive>
+    void serialize(Archive& ar) {
+      ar(size_, count_);
+      for (std::size_t i=0; i<size_; ++i) ar(buffer_[i]);
+      ar(flag_);
+    }
+    template <typename Archive>
+    static void load_and_construct(Archive& ar, cereal::construct<PendingRequest>& construct) {
+      std::size_t size;
+      int count;
+      ar(size, count);
+
+      construct(std::move(make_unique<uint8_t[]>(size)), size, count);
+
+      for (std::size_t i=0; i<size; ++i) ar(construct->buffer_[i]);
+      
+      ar(construct->flag_);
+    }
 };
 
 struct MessageQueue {
@@ -94,6 +168,20 @@ struct MessageQueue {
     std::mutex msg_list_lock_;
 
     std::vector<std::unique_ptr<PendingRequest>> pending_request_list_;
+
+    template <typename Archive>
+    void serialize(Archive& ar) {
+      ar(max_buffer_size_, msg_list_, pending_request_list_);
+    }
+    template <typename Archive>
+    static void load_and_construct(Archive& ar, cereal::construct<MessageQueue>& construct) {
+      unsigned int max_buffer_size;
+      ar(max_buffer_size);
+
+      construct(max_buffer_size);
+
+      ar(construct->msg_list_, construct->pending_request_list_);
+    }
 };
 
 } // namespace warped
