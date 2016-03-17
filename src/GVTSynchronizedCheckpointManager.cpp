abe84e928033c1cb1e97225566aafac290bed592
o8r <my+github@o8r.info>
2016-03-17 16:43:42 +0900

    Fix GVTSynchronizedCheckpointManager::checkpointRequired

--- src/GVTSynchronizedCheckpointManager.cpp
+++ src/GVTSynchronizedCheckpointManager.cpp
@@ -66,7 +66,7 @@ warped::GVTSynchronizedCheckpointManager::onGVT(unsigned int /*gvt*/)
 bool
 warped::GVTSynchronizedCheckpointManager::checkpointRequired() const
 {
-  return pimpl_->checkpointRequired.load();
+  return pimpl_->checkpointRequired.load() == 0;
 }
 
 void
