504a881aa1333cecb2a04b1bc170d45bb6ba175c
o8r <my+github@o8r.info>
2016-03-28 18:05:51 +0900

    Add src/Event.cpp

---
+++ src/Event.cpp
@@ -0,0 +1,6 @@
+#include "Event.hpp"
+
+// defined in TimeWarpEventDispatcher.cpp
+// WARPED_REGISTER_POLYMORPHIC_SERIALIZABLE_CLASS(warped::NegativeEvent)
+
+WARPED_REGISTER_POLYMORPHIC_SERIALIZABLE_CLASS(warped::InitialEvent)
